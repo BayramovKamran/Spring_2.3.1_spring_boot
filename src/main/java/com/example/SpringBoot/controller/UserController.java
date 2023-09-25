8c2e62bac97a90a679d694bcb4a7e11d7dd8c90d
Kamran <kamran.07@icloud>
2023-09-25 16:30:57 +0300

    first commit

--- src/main/java/com/example/SpringBoot/controller/UserController.java
+++ src/main/java/com/example/SpringBoot/controller/UserController.java
@@ -4,7 +4,11 @@ import com.example.SpringBoot.model.User;
 import com.example.SpringBoot.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
